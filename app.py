e0e82110ab8c17f83c6303189b031325938ee500
Ronny <ronaldsrundans@gmail.com>
2026-02-28 16:58:30 +0200

    Update app.py
    Fix app.py

--- app.py
+++ app.py
@@ -4,12 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    #return 'Hello World'
     connect = sqlite3.connect('catalog.db')
     cursor = connect.cursor()
     cursor.execute('SELECT * FROM CATALOG')
     data = cursor.fetchall()
     return render_template("catalog.html", data=data)
-if __name__ == '__main__':
-    app.run()
+
 
